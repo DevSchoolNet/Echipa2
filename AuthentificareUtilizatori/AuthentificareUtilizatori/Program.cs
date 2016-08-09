f5016edb594646465ac153b6cabf287c0c32fdeb
DaniRvd <danrvd16@gmail.com>
2016-08-09 17:49:07 +0300

    asa

--- AuthentificareUtilizatori/AuthentificareUtilizatori/Program.cs
+++ AuthentificareUtilizatori/AuthentificareUtilizatori/Program.cs
@@ -12,10 +12,15 @@ namespace AuthentificareUtilizatori
         {
             string name = "\0";
             string password = "\0";
+            string met = "\0";
             Console.WriteLine("Username:");
             name = Console.ReadLine();
-            Console.WriteLine("Password");
+            Console.WriteLine("Password:);
             password = Console.ReadLine();
+            Console.WriteLine("Metoda de logare:");
+            met = Console.ReadLine();
+            var user = new Utilizator(name, password,met);
+
 
         }
     }
